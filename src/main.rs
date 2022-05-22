5aff36d6d7c80456f13e483fd6d0d9b55b37f26b
Janne Sauvala <janne.sauvala@gmail.com>
2022-05-22 13:46:23 +0300

    Add prompt loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::env;
+use std::io::Write;
+use std::io;
+
 fn main() {
-		println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+        println!("Usage: rlox [script]");
+        std::process::exit(64);
+    } else if args.len() == 1 {
+        run_prompt();
+    }
+}
+
+fn run_prompt() {
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+        let mut line = String::new();
+        io::stdin()
+            .read_line(&mut line)
+            .expect("Failed to read line");
+
+        if line.len() == 0 {
+            println!("Bye!");
+            break;
+        }
+
+        println!("{}", line)
+    }
 }
